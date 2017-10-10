ca4a9352fe9f65bcca15d92b593184aee7c794a2
Kent Sommer <kent.sommer13@gmail.com>
2017-10-10 16:16:31 +0900

    Fixed Files

---
+++ download_dataset_and_weights.sh
@@ -0,0 +1,5 @@
+wget https://github.com/kentsommer/VGG16-Image-Retrieval/releases/download/v1.0/Dataset_Directory.zip
+wget https://github.com/kentsommer/VGG16-Image-Retrieval/releases/download/v1.0/vgg16_weights.npz
+
+unzip Dataset_Directory.zip
+rm -rf Dataset_Directory.zip
